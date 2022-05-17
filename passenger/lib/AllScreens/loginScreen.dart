99b4719467350e1277eb4f5ac88de6a10cbd699f
Abhilash-Puthukkudi <abhilashp44@gmail.com>
2022-05-17 20:18:09 +0530

    login screen design completed

--- passenger/lib/AllScreens/loginScreen.dart
+++ passenger/lib/AllScreens/loginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class loginScreen extends StatefulWidget {
   const loginScreen({Key? key}) : super(key: key);
@@ -11,72 +12,95 @@ class _loginScreenState extends State<loginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(223, 24, 23, 23),
-      body: Column(
-        children: [
-          SizedBox(height: 65.0),
-          Image(
-            image: AssetImage("images/icon.png"),
-            width: 390,
-            height: 250,
-            alignment: Alignment.center,
-          ),
-          SizedBox(height: 1.0),
-          Text(
-            "Login as a Passenger",
-            style: TextStyle(
-                color: Colors.yellow, fontSize: 24.0, fontFamily: "Brand Bold"),
-            textAlign: TextAlign.center,
-          ),
-          Padding(
-            padding: EdgeInsets.all(30),
-            child: Column(
-              children: [
-                SizedBox(height: 1.0),
-                TextField(
-                  style: TextStyle(color: Colors.yellow),
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      labelText: "Email",
-                      labelStyle:
-                          TextStyle(fontSize: 14.0, color: Colors.white),
-                      hintStyle: TextStyle(
-                          fontSize: 10.0,
-                          color: Color.fromARGB(255, 255, 255, 255))),
-                ),
-                SizedBox(height: 1.0),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: "Password",
-                      labelStyle:
-                          TextStyle(fontSize: 14.0, color: Colors.white),
-                      hintStyle: TextStyle(fontSize: 10.0, color: Colors.grey)),
-                ),
-                SizedBox(height: 20.0),
-                ElevatedButton(
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                              side: BorderSide(color: Colors.red)))),
-                  onPressed: () {},
-                  child: Container(
-                    height: 50,
-                    child: Center(
-                      child: Text(
-                        "Login",
-                        style:
-                            TextStyle(fontSize: 18.0, fontFamily: "Brand Bolt"),
+      backgroundColor: Color.fromARGB(223, 34, 34, 34),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 70.0),
+            Image(
+              image: AssetImage("images/icon.png"),
+              width: 390,
+              height: 250,
+              alignment: Alignment.center,
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "Login as a Passenger",
+              style: TextStyle(
+                  color: Colors.yellow,
+                  fontSize: 28.0,
+                  fontFamily: "Brand Bold"),
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                children: [
+                  SizedBox(height: 1.0),
+                  TextField(
+                    style: TextStyle(color: Colors.yellow, fontSize: 18),
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.white,
+                        ),
+                        hintStyle: TextStyle(
+                            fontSize: 10.0,
+                            color: Color.fromARGB(255, 255, 255, 255))),
+                  ),
+                  SizedBox(height: 5.0),
+                  TextField(
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.yellow, fontSize: 18),
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Password",
+                        labelStyle:
+                            TextStyle(fontSize: 14.0, color: Colors.white),
+                        hintStyle:
+                            TextStyle(fontSize: 10.0, color: Colors.grey)),
+                  ),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(
+                                        color: Color.fromARGB(255, 8, 7, 7))))),
+                    onPressed: () {},
+                    child: Container(
+                      height: 50,
+                      child: Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                              fontSize: 18.0, fontFamily: "Brand Bolt"),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Do not have an Account? Register Here.",
+                  style: TextStyle(
+                      color: Colors.yellow,
+                      fontSize: 14.0,
+                      fontFamily: "Brand Bold"),
+                  textAlign: TextAlign.center,
+                ))
+          ],
+        ),
       ),
     );
   }
