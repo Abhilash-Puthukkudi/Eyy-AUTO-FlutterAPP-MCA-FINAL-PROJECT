6fab3e5a2960100f51d023d57c3a845c41040b48
Abhilash-Puthukkudi <abhilashp44@gmail.com>
2022-05-18 20:07:27 +0530

    divider widget created

--- passenger/lib/allwidgets/dividerWidget.dart
+++ passenger/lib/allwidgets/dividerWidget.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+class DividorWidget extends StatelessWidget {
+  const DividorWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Divider(
+      height: 1.0,
+      color: Colors.black,
+      thickness: 1.0,
+    );
+  }
+}
