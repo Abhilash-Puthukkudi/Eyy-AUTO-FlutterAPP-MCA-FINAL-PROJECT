23b8c5b16e7e7e2bcce1c1663521e6978993f38a
Abhilash-Puthukkudi <abhilashp44@gmail.com>
2022-05-18 20:13:09 +0530

    dividor removed to fix bottom pixel error

--- passenger/lib/AllScreens/mainScreen.dart
+++ passenger/lib/AllScreens/mainScreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:passenger/allwidgets/dividerWidget.dart';
 
 class mainScreen extends StatefulWidget {
   const mainScreen({Key? key}) : super(key: key);
