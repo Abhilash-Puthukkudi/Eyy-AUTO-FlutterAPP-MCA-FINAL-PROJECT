9a974b882d1c387221bbfcb0fd15a896ab73f942
Abhilash-Puthukkudi <abhilashp44@gmail.com>
2022-05-19 02:23:39 +0530

    provider added

--- passenger/lib/assistance/assistanceMethods.dart
+++ passenger/lib/assistance/assistanceMethods.dart
@@ -4,6 +4,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:passenger/assistance/requestAssistance.dart';
 import 'package:passenger/functions/configMaps.dart';
+import 'package:passenger/models/address.dart';
 
 class assistanceMethods {
   static Future<String> searchCordinateAddress(Position position) async {
@@ -16,6 +17,11 @@ class assistanceMethods {
 
     if (response != "failed") {
       placeAddress = response["results"][0]["formatted_address"];
+
+      Address passengerPickupadress = new Address();
+      passengerPickupadress.longitude = position.latitude;
+      passengerPickupadress.longitude = position.longitude;
+      passengerPickupadress.placeName = placeAddress;
     }
 
     return placeAddress;
