6cd25c713dee02f2b73763fd2dddcfc55dd46684
Abhilash-Puthukkudi <abhilashp44@gmail.com>
2022-05-19 10:23:38 +0530

    adress clss modified

--- passenger/lib/main.dart
+++ passenger/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:passenger/AllScreens/loginScreen.dart';
 import 'package:passenger/AllScreens/mainScreen.dart';
 import 'package:passenger/AllScreens/registrationScreen.dart';
+import 'package:passenger/DataHandler/appData.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -20,16 +22,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.yellow),
-      initialRoute: mainScreen.idScreen,
-      routes: {
-        registrationScreen.idScreen: (context) => registrationScreen(),
-        loginScreen.idScreen: (context) => loginScreen(),
-        mainScreen.idScreen: (context) => mainScreen()
-      },
+    return ChangeNotifierProvider(
+      create: (context) => appData(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.yellow),
+        initialRoute: mainScreen.idScreen,
+        routes: {
+          registrationScreen.idScreen: (context) => registrationScreen(),
+          loginScreen.idScreen: (context) => loginScreen(),
+          mainScreen.idScreen: (context) => mainScreen()
+        },
+      ),
     );
   }
 }
