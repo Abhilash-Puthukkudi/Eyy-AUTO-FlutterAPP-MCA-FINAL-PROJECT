5154a5c9a02b08d3d8d3476c0886e0a9547c90f2
Abhilash-Puthukkudi <abhilashp44@gmail.com>
2022-07-04 17:44:23 +0530

    working on notification screen

---
+++ driver/lib/notifications/notificationDialouge.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotificationDialouge extends StatefulWidget {
+  const NotificationDialouge({Key? key}) : super(key: key);
+
+  @override
+  State<NotificationDialouge> createState() => _NotificationDialougeState();
+}
+
+class _NotificationDialougeState extends State<NotificationDialouge> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
