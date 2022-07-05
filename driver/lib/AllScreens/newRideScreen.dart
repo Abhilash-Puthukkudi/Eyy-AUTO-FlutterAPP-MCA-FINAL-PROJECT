2ca7575ae58fdda77b3df83866de564d246c398c
Abhilash-Puthukkudi <abhilashp44@gmail.com>
2022-07-05 14:23:02 +0530

    user interface of new ride screen finshed

--- driver/lib/AllScreens/newRideScreen.dart
+++ driver/lib/AllScreens/newRideScreen.dart
@@ -1,24 +1,178 @@
+import 'dart:async';
+
 import 'package:driver/models/rideDetails.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class newRideScreen extends StatefulWidget {
   // const newRideScreen({Key? key}) : super(key: key);
   final RideDetails? rideDetails;
   newRideScreen({this.rideDetails});
 
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   State<newRideScreen> createState() => _newRideScreenState();
 }
 
 class _newRideScreenState extends State<newRideScreen> {
+  Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  GoogleMapController? newRideGoogleMapController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("New Ride"),
-      ),
-      body: Center(
-        child: Text("This is new ride page"),
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: newRideScreen._kGooglePlex,
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
+            // zoomGesturesEnabled: true,
+            // zoomControlsEnabled: true,
+            onMapCreated: (GoogleMapController controller) {
+              _controllerGoogleMap.complete(controller);
+              newRideGoogleMapController = controller;
+            },
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16.0),
+                      topRight: Radius.circular(16.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black38,
+                      blurRadius: 16.0,
+                      spreadRadius: 0.5,
+                      offset: Offset(0.7, 0.7),
+                    )
+                  ]),
+              height: 260.0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0, vertical: 18.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "10 minutes",
+                      style: TextStyle(
+                          fontFamily: "Brand Bold",
+                          fontSize: 14.0,
+                          color: Colors.deepPurple),
+                    ),
+                    SizedBox(
+                      height: 6.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Abhilash Puthukkudi",
+                          style: TextStyle(
+                            fontFamily: "Brand Bold",
+                            fontSize: 24.0,
+                          ),
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(right: 16.0),
+                            child: Icon(Icons.call)),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 26.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "images/pickicon.png",
+                          height: 16.0,
+                          width: 16.0,
+                        ),
+                        SizedBox(
+                          width: 18.0,
+                        ),
+                        Expanded(
+                            child: Container(
+                          child: Text(
+                            "Chelavoor kozhikode 673571",
+                            style: TextStyle(fontSize: 18.0),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ))
+                      ],
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "images/desticon.png",
+                          height: 16.0,
+                          width: 16.0,
+                        ),
+                        SizedBox(
+                          width: 18.0,
+                        ),
+                        Expanded(
+                            child: Container(
+                          child: Text(
+                            "kunnamnagalam,kozhikode,673571",
+                            style: TextStyle(fontSize: 18.0),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ))
+                      ],
+                    ),
+                    SizedBox(
+                      height: 26.0,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Arrived",
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              Icon(
+                                Icons.directions_walk,
+                                color: Colors.white,
+                                size: 26.0,
+                              )
+                            ],
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.blueAccent,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 20),
+                              textStyle: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold))),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
